d0f85b074f05a2bf0379ba8feed03746c91e8989
Nils Jaekel <nils.jaekel@davils.com>
2026-04-12 20:25:00 +0200

    chore: configure KSP and Kotest in kore-core.gradle.kts

--- buildSrc/src/main/kotlin/kore-core.gradle.kts
+++ buildSrc/src/main/kotlin/kore-core.gradle.kts
@@ -1,8 +1,53 @@
 plugins {
     org.jetbrains.kotlin.multiplatform
+    com.google.devtools.ksp
     com.davils.kreate
+    io.kotest
+}
+
+kreate {
+    project {
+        name = "davilsx-kore"
+        description = ""
+
+        docs {
+            enabled = true
+            moduleName = "Kore"
+            outputDirectory = "dokka"
+        }
+
+        tests {
+            enabled = true
+            maxParallelForks = Runtime.getRuntime().availableProcessors()
+            ignoreFailures = false
+            alwaysRunTests = false
+            failOnNoDiscoveredTests = false
+
+            logging {
+                logPassedTests = true
+                logSkippedTests = true
+                logTestStarted = true
+            }
+
+            report {
+                enabled = true
+                xml = true
+                html = false
+            }
+        }
+    }
+
+    platform {
+        javaVersion = JavaVersion.VERSION_25
+        explicitApi = true
+        allWarningsAsErrors = true
+    }
 }
 
 kotlin {
     jvm()
+
+    js(IR) {
+        browser()
+    }
 }
