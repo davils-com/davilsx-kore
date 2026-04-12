302f40b76637e4e005c3cfbf59e5091e1110150d
Nils Jaekel <nils.jaekel@davils.com>
2026-04-12 20:25:05 +0200

    chore: add test dependencies for common and JVM source sets in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,18 @@ plugins {
     `kore-core`
 }
 
+kotlin {
+    sourceSets {
+        commonTest {
+            dependencies {
+                implementation(libs.bundles.kore.tests.common.impl)
+            }
+        }
+
+        jvmTest {
+            dependencies {
+                implementation(libs.bundles.kore.tests.jvm.impl)
+            }
+        }
+    }
+}
