511569a15a952be4976e05e0320dc192e5e566a6
Nils Jaekel <nils.jaekel@davils.com>
2026-04-13 15:40:23 +0200

    chore: add dependencies for commonMain source set in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,13 @@ plugins {
 
 kotlin {
     sourceSets {
+        commonMain {
+            dependencies {
+                implementation(libs.bundles.kore.common.impl)
+                api(libs.bundles.kore.common.api)
+            }
+        }
+
         commonTest {
             dependencies {
                 implementation(libs.bundles.kore.tests.common.impl)
