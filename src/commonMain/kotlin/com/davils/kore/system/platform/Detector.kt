8f5d76a879a754933e45e8ef4aae2736a902528c
Nils Jaekel <nils.jaekel@davils.com>
2026-04-28 11:10:21 +0200

    feat: add OsDetector object for operating system detection

---
+++ src/commonMain/kotlin/com/davils/kore/system/platform/Detector.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2026 Davils
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+
+package com.davils.kore.system.platform
+
+/**
+ * Internal object for detecting the operating system and platform details.
+ *
+ * @since 1.0.0
+ */
+internal expect object OsDetector {
+    /**
+     * Indicates whether the current platform is JVM.
+     *
+     * @since 1.0.0
+     */
+    val isJvm: Boolean
+
+    /**
+     * The current operating system.
+     *
+     * @since 1.0.0
+     */
+    val os: Os
+}
