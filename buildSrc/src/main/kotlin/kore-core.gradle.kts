bb5dfea4a64f45b2010a6388615fd1b4c5ed3428
Nils Jaekel <nils.jaekel@davils.com>
2026-04-30 19:35:46 +0200

    feat: enhance project metadata and enable publishing configuration in kore-core.gradle.kts

--- buildSrc/src/main/kotlin/kore-core.gradle.kts
+++ buildSrc/src/main/kotlin/kore-core.gradle.kts
@@ -7,13 +7,14 @@ plugins {
     org.jetbrains.kotlin.multiplatform
     com.google.devtools.ksp
     com.davils.kreate
+    `maven-publish`
     io.kotest
 }
 
 kreate {
     project {
         name = "davilsx-kore"
-        description = ""
+        description = " The core library of the DavilsX ecosystem, providing essential utilities and abstractions for Kotlin Multiplatform projects. "
 
         docs {
             enabled = true
@@ -38,6 +39,56 @@ kreate {
                 enabled = true
                 xml = true
                 html = false
+            }
+        }
+
+        publish {
+            enabled = true
+            inceptionYear = 2026
+            website = "https://github.com/davils-com/davilsx-kore"
+
+            pom {
+                issueManagement {
+                    system = "Github Issues"
+                    url = "https://github.com/davils-com/davilsx-kore/issues"
+                }
+
+                ciManagement {
+                    system = "Github Actions"
+                    url = "https://github.com/davils-com/davilsx-kore/actions"
+                }
+
+                licenses {
+                    license {
+                        name = "Apache 2.0"
+                        url = "https://github.com/davils-com/davilsx-kore/blob/main/LICENSE"
+                        distribution = "repo"
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = "davils"
+                        name = "Davils"
+                        email = "development@davils.com"
+                        organization = "Davils"
+                        timezone = "Europe/Berlin"
+                    }
+                }
+
+                scm {
+                    url = "https://github.com/davils-com/davilsx-kore.git"
+                    connection = "scm:git:https://github.com/davils-com/davilsx-kore.git"
+                    developerConnection = "scm:git@github.com:davils-com/davilsx-kore.git"
+                }
+            }
+
+            repositories {
+                mavenCentral {
+                    enabled = true
+                    signPublications = true
+                    automaticRelease = true
+                }
             }
         }
     }
